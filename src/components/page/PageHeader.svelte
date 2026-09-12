f4f2bd7f688df3286d677e677bd2395ea3552d04
kureichi <hibikumonogatari@gmail.com>
2026-09-13 00:06:53 +0700

    fix(components/pageheader): add container root and body to component

--- src/components/page/PageHeader.svelte
+++ src/components/page/PageHeader.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
+	import ContainerBody from '$components/container/ContainerBody.svelte';
+	import ContainerRoot from '$components/container/ContainerRoot.svelte';
+
 	let { title = 'Sample Title', description = 'This is description that use in this page.' } =
 		$props();
 </script>
 
-<div class="flex h-40 w-full flex-col items-center justify-center">
-	<h1 class="text-5xl font-bold">
-		{title}
-	</h1>
-	<p class="opacity-80">
-		{description}
-	</p>
-</div>
+<ContainerRoot>
+	<ContainerBody>
+		<div class="flex h-40 w-full flex-col items-center justify-center">
+			<h1 class="text-5xl font-bold">
+				{title}
+			</h1>
+			<p class="opacity-80">
+				{description}
+			</p>
+		</div>
+	</ContainerBody>
+</ContainerRoot>
