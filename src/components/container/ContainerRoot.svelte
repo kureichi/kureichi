beeb7fec72c1221a857002a9c4bf76c78f712a81
kureichi <hibikumonogatari@gmail.com>
2026-09-13 11:52:47 +0700

    fix(components/container): make container root height full or follow the div parent

--- src/components/container/ContainerRoot.svelte
+++ src/components/container/ContainerRoot.svelte
@@ -2,6 +2,6 @@
 	let { children } = $props();
 </script>
 
-<div class="flex w-full items-start justify-center px-5">
+<div class="flex h-full w-full items-start justify-center px-5">
 	{@render children()}
 </div>
