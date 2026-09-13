3a1cf389e77f5915ac8d0fda960330cc59b461b7
kureichi <hibikumonogatari@gmail.com>
2026-09-13 14:03:25 +0700

    fix(route/work): fix svelte mark this page as prerenderable

--- src/routes/work/[ytID]/+page.ts
+++ src/routes/work/[ytID]/+page.ts
@@ -1,6 +1,8 @@
 import type { WorkType } from '$src/lib/type';
 import type { PageLoad } from './$types';
 
+export const prerender = false;
+
 export const load: PageLoad = async ({ params, parent }) => {
 	const layoutData = await parent();
 	const { ytID } = params;
