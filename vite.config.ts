7b1f6901c8fd58a613b08da1c222404098a4a1eb
kureichi <hibikumonogatari@gmail.com>
2026-09-18 15:03:22 +0700

    chore: change adapter static to adapter cloudflare in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
@@ -21,13 +21,7 @@ export default defineConfig({
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter({
-				pages: 'build',
-				assets: 'build',
-				fallback: '404.html',
-				precompress: false,
-				strict: true
-			})
+			adapter: adapter()
 		})
 	]
 });
