1b848a9591b76902def9c969129da2e0a5b25804
kureichi <hibikumonogatari@gmail.com>
2026-09-18 13:42:39 +0700

    feat: add isObject util to check object type

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,7 @@
 export function parseJSON<T>(jsonString: string): T {
 	return JSON.parse(jsonString);
 }
+
+export function isObject(value: unknown): value is Record<string, unknown> {
+  return typeof value === 'object' && value !== null;
+}
