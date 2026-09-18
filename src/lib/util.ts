1d202d5da54373d729786bbd314192aa0b62b32d
kureichi <hibikumonogatari@gmail.com>
2026-09-18 14:56:42 +0700

    feat: add getTitle util function to get title easily

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -3,5 +3,14 @@ export function parseJSON<T>(jsonString: string): T {
 }
 
 export function isObject(value: unknown): value is Record<string, unknown> {
-  return typeof value === 'object' && value !== null;
+	return typeof value === 'object' && value !== null;
+}
+
+export function getTitle(context: string = '') {
+	const title = 'Kureichii Works';
+	if (context == '') {
+		return title;
+	}
+
+	return title + ' - ' + context;
 }
