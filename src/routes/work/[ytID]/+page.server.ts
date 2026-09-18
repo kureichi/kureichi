0e06d2df2b66218be5c59f0f6c90575da0b684c7
kureichi <hibikumonogatari@gmail.com>
2026-09-18 13:52:19 +0700

    fix: change ytId key in work page to youtubeVideoID

--- src/routes/work/[ytID]/+page.server.ts
+++ src/routes/work/[ytID]/+page.server.ts
@@ -5,7 +5,7 @@ export const load: PageServerLoad = async ({ params }) => {
 	const { ytID } = params;
 	const data = await getData();
 
-	const workFilter = data.workList.filter((m) => m.ytId == ytID);
+	const workFilter = data.workList.filter((m) => m.youtubeVideoID == ytID);
 	const workData = workFilter.pop();
 
 	return {
